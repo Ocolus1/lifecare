32b7fea0e187c5835c317ad59a1140642b975f96
Cypherspot <awolesiboluwatife@gmail.com>
2021-01-08 23:01:50 +0100

    remove the life

--- life/main/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Feedback
-
-
-class feedbackSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Feedback
-        fields = "__all__"
